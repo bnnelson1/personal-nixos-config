f2c88eb93d768bb788f1c96e06cd45bf02d5f398
bnnelson1 <122384363+bnnelson1@users.noreply.github.com>
2024-10-13 17:55:31 -0400

    Initial.

---
+++ development/default.nix
@@ -0,0 +1,38 @@
+{ pkgs, ... }:
+  
+let 
+  vscodeWithExtensions = pkgs.vscode-with-extensions.override {
+      vscodeExtensions = with pkgs.vscode-extensions; [
+        catppuccin.catppuccin-vsc
+        catppuccin.catppuccin-vsc-icons
+        jnoortheen.nix-ide
+      ];
+    };
+in
+{
+  # Stable version is 2.1.2, 
+  #   but 2.3.2 disabled an arguably annoying warning involving 'with'.
+  # So, this overlay is to override the stable package and retrieve
+  #   the 2.3.2 version instead.
+  nixpkgs.overlays = [
+    (final: prev: {
+      nixd = prev.nixd.overrideAttrs (old: {
+        version = "2.3.2";
+        src = final.fetchFromGitHub {
+          owner = "nix-community";
+          repo = "nixd";
+          rev = "2.3.2";
+          hash = "sha256-ffHLKHpqgVlYLGQ/Dc/6hW/inA98QdMJiv/fT2IrH7c=";
+        };
+      });
+    })
+  ];
+
+  environment.systemPackages = with pkgs; [ 
+    nixd # Nix language server
+    vscodeWithExtensions
+  ];
+
+}
+
+
---
+++ themes/catppuccin.nix
@@ -0,0 +1,35 @@
+{ pkgs, fetchurl, ... }: 
+
+{  
+  environment.systemPackages = with pkgs; [
+    (catppuccin-kde.override {
+      flavour = [ "latte" "macchiato" ];
+      accents = [ "rosewater" "blue" ];
+    })
+
+    (catppuccin-sddm.override {
+      flavor = "macchiato";
+      background = "${./backgrounds/shaded_landscape.png}";
+      loginBackground = true;
+    })
+    (catppuccin-sddm.override {
+      flavor = "latte";
+    })
+  ]
+  ++ (
+    with catppuccin-cursors; [
+      latteRosewater
+      latteLight
+      
+      macchiatoRosewater
+      macchiatoBlue
+      macchiatoDark
+    ]
+  );
+
+  services.displayManager.sddm = {
+    theme = "catppuccin-macchiato";
+  };
+
+}
+
